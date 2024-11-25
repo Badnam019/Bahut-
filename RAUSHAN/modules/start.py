399234b612a14f695472bda341414ed714f709a7
istkharalam6202 <istkharalam6202@gmail.com>
2024-11-25 20:34:07 +0530

    Update start.py

--- RAUSHAN/modules/start.py
+++ RAUSHAN/modules/start.py
@@ -33,7 +33,7 @@ async def start(_, m: Message):
         await asyncio.sleep(1.3)
         await accha.edit("__ʜᴇʟʟᴏ ʙᴀʙʏ..__")
         await asyncio.sleep(0.2)
-        await accha.edit("__ɪ'ᴍ sᴏɴᴀʟɪ..__")
+        await accha.edit("__ɪ'ᴍ 𝐏υяꪜɪ..__")
         await asyncio.sleep(0.2)
         await accha.edit("__sᴛᴀʀᴛɪɴɢ..__")
         await asyncio.sleep(0.2)
